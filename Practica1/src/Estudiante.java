4a23ef4936e617ffdaee6667817332647d905b51
milo70va <53384395+milo70va@users.noreply.github.com>
2021-03-15 23:14:53 -0500

    Update Estudiante.java

--- Practica1/src/Estudiante.java
+++ Practica1/src/Estudiante.java
@@ -8,15 +8,43 @@ public class Estudiante extends Persona implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 5484708416456237025L;
+	private double promedio;
 
-	public Estudiante(Materia[] materias, String nombre, char sexo, 
-			int edad, int faltas,String TarjetaDeIdentidad) {
-		super(materias, nombre, sexo, edad, faltas,TarjetaDeIdentidad);
-		// TODO Auto-generated constructor stub
+	public double getPromedio() {
+		return promedio;
+	}
+
+	public void setPromedio(double promedio) {
+		this.promedio = promedio;
+	}
+
+	public Estudiante(int id, String contraseña, Materia[] materias, String nombre, char sexo, int edad, int faltas,String TarjetaDeIdentidad) {
+		super(id, contraseña, materias, nombre, sexo, edad, faltas, TarjetaDeIdentidad);
 	}
 
 	public Estudiante() {
 		
+	}
+
+	public double calcularProm(Materia[] materias, int contadorMaterias, int contadorNotas, double prom) {
+		if(contadorMaterias > materias.length-1) {
+			setPromedio(prom);
+			return prom/contadorMaterias-1;
+		} else {
+			if(contadorNotas > materias[contadorMaterias].getNota().length-1) {
+				return calcularProm(materias, contadorMaterias+1, 0, prom);
+			} else {
+				return materias[contadorMaterias].getNota()[contadorNotas] + calcularProm(materias, contadorMaterias, contadorNotas+1, prom);
+			}
+		}
+	}
+	
+	public double calcularPromMateria(Materia materia, int contadorNotas, double prom) {
+		if(contadorNotas < materia.getNota().length-1) {
+			return prom;
+		} else {
+			return prom + materia.getNota()[contadorNotas];
+		}
 	}
 
 
