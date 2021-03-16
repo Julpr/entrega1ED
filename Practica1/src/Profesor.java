5d23bd124d3c5a0ef9b75da1a6abe17c1eaca96c
milo70va <53384395+milo70va@users.noreply.github.com>
2021-03-15 23:16:24 -0500

    Update Profesor.java

--- Practica1/src/Profesor.java
+++ Practica1/src/Profesor.java
@@ -6,11 +6,44 @@ public class Profesor extends Persona implements Serializable{
 
 	private static final long serialVersionUID = -4262531568827767469L;
 
-	public Profesor(Materia[] materias, String nombre, char sexo, int edad, int faltas,String cedula) {
-		super(materias, nombre, sexo, edad, faltas,cedula);
-		// TODO Auto-generated constructor stub
+	private double sueldo;
+	private double estudios;
+	
+
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public double getEsudios() {
+		return estudios;
+	}
+
+	public void setEsudios(double esudios) {
+		this.estudios = esudios;
+	}
+
+	public Profesor(int id, String contraseña, Materia[] materias, String nombre, char sexo, int edad, int faltas,String cedula, double sueldo, double estudios) {
+		super(id, contraseña, materias, nombre, sexo, edad, faltas,cedula);
+		this.sueldo = sueldo;
+		this.estudios = estudios;
 	}
 	
+	public void calcularSueldo(int id) throws Excepciones {
+		int contador = 0;
+		for(int i = 0; i < ((Persona) buscarUsuario(id)).getMaterias().length-1; i++) {
+			contador += ((Persona) buscarUsuario(id)).getMaterias()[i].getCreditos();
+		}
+		setSueldo(contador*((Profesor) buscarUsuario(id)).getEsudios()*100.000);
+		//estudios es el procentaje extra segun los titulos
+	}
+
+	public void agregarNota(int idEstudiante, double nota, String materia) throws Excepciones {
+		buscarUsuario(idEstudiante).buscarMateria(idEstudiante, materia).agregarNota(materia, nota);
+	}
 	
 
 }
