0d41727303e0453fba9eb9d92b08a94187e10d82
JulianPat <JulianPat@192.168.1.57>
2021-03-15 20:28:43 -0500

    Interfaz admon corregida

--- Practica1/src/Interfaz_admon.java
+++ Practica1/src/Interfaz_admon.java
@@ -1,4 +1,3 @@
-
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,17 +6,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 
 public class Interfaz_admon {
 
-	private JFrame frame;
+	private JFrame frame_admon;
+	private JButton btnAdmon_newProfesor;
+	private JLabel lblNewLabel;
+	
+	
 
-
-
-	public JFrame getFrame() {
-		return frame;
+	public JFrame getFrame_admon() {
+		return frame_admon;
 	}
 
 	/**
@@ -28,7 +30,7 @@ public class Interfaz_admon {
 			public void run() {
 				try {
 					Interfaz_admon window = new Interfaz_admon();
-					window.frame.setVisible(true);
+					window.frame_admon.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,47 +49,59 @@ public class Interfaz_admon {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 380, 446);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame_admon = new JFrame();
+		frame_admon.setBounds(100, 100, 380, 446); //-, - , ancho, alto
+		frame_admon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame_admon.getContentPane().setLayout(null);
 		
-		JButton btnLogin_addProf = new JButton("<html><center>Nuevo Profesor");
-		btnLogin_addProf.setBounds(132, 136, 117, 40);
-		frame.getContentPane().add(btnLogin_addProf);
-		btnLogin_addProf.addActionListener(new ActionListener() {
-
+		lblNewLabel = new JLabel("User: Administrador");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblNewLabel.setBounds(79, 52, 199, 25);
+		frame_admon.getContentPane().add(lblNewLabel);
+		
+		JButton btnLogin_addProf;
+		btnAdmon_newProfesor = new JButton("<html><center>Nuevo Profesor");
+		btnAdmon_newProfesor.setBounds(129, 113, 95, 44);
+		frame_admon.getContentPane().add(btnAdmon_newProfesor);
+		btnAdmon_newProfesor.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Interfaz_NewProfesor addprofe= new Interfaz_NewProfesor();
 				addprofe.getFrame_newProfesor().setVisible(true);
-				Interfaz_admon.this.frame.dispose();
+				Interfaz_admon.this.frame_admon.dispose();
 
 			}
 		});
+		
+		JButton btnAdmon_newEstudiante = new JButton("<html><center>Nuevo Estudiante");
+		btnAdmon_newEstudiante.setBounds(129, 209, 95, 44);
+		frame_admon.getContentPane().add(btnAdmon_newEstudiante);
+		btnAdmon_newEstudiante.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Interfaz_NewEstudiante addestudiante= new Interfaz_NewEstudiante();
+				addestudiante.getFrame_newEstudiante().setVisible(true);
+				Interfaz_admon.this.frame_admon.dispose();
 
-		JButton btnLogin_ingresar = new JButton("<html><center>Nuevo estudiante");
-		btnLogin_ingresar.setBounds(132, 244, 117, 40);
-		frame.getContentPane().add(btnLogin_ingresar);
+			}
+		});
 		
-		JLabel lblAdmon_Titulo = new JLabel("User: Administrador");
-		lblAdmon_Titulo.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblAdmon_Titulo.setBounds(79, 52, 199, 25);
-		frame.getContentPane().add(lblAdmon_Titulo);
+		JButton btnLogin_cancelar = new JButton("Cerrar sesión");
+		btnLogin_cancelar.setForeground(Color.RED);
+		btnLogin_cancelar.setBounds(129, 338, 117, 29); //x, y, alto, ancho
+		frame_admon.getContentPane().add(btnLogin_cancelar);
 		
-		JButton btnAdmon_CerrarSesion = new JButton("<html><center>Nuevo estudiante");
-		btnAdmon_CerrarSesion.setBounds(132, 329, 117, 40);
-		frame.getContentPane().add(btnAdmon_CerrarSesion);
-		btnLogin_ingresar.addActionListener(new ActionListener() {
+		
+		btnLogin_cancelar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Interfaz_NewEstudiante addEstudiante= new Interfaz_NewEstudiante();
-				addEstudiante.getFrame_newEstudiante().setVisible(true);
-				Interfaz_admon.this.frame.dispose();
-
+				Interfaz_Login addestudiante= new Interfaz_Login();
+				addestudiante.getFrame_Login().setVisible(true);
+				Interfaz_admon.this.frame_admon.dispose();
 			}
 		});
-
+		
+		
 	}
 }
